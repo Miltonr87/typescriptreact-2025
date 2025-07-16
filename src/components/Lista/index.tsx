f2051497b4a49758a09420352f00ee66169cf1be
Miltonr87 <miltonrodrigues713@gmail.com>
2025-07-16 18:21:41 -0300

    Tested

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+// remote testing
+
 function Lista() {
-  const tarefas = [{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }];
+  const tarefas = [
+    {
+      tarefa: 'React',
+      tempo: '02:00:00',
+    },
+    {
+      tarefa: 'Javascript',
+      tempo: '01:00:00',
+    },
+    {
+      tarefa: 'Typescript',
+      tempo: '03:00:00',
+    },
+  ];
   return (
     <aside>
       <h2> Estudos do dia </h2>
@@ -23,7 +29,7 @@ function Lista() {
         ))}
       </ul>
     </aside>
-  )
+  );
 }
 
 export default Lista;
